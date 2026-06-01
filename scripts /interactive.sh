eea5467742a4eceeabfb6c2755b6f32d6d2e7130
mithil <mithilsalunkhe@outlook.com>
2026-06-01 10:03:29 -0500

    add interactive and setup scripts for environment configuration

---
+++ scripts /interactive.sh
@@ -0,0 +1,2 @@
+srun -A bgnp-dtai-gh --time=02:00:00 --nodes=1 --ntasks-per-node=32 --partition=ghx4-interactive --gpus=1 --mem=25
+6g --pty /bin/bash 
---
+++ scripts /setup.sh
@@ -0,0 +1,2 @@
+module load python/3.11.9 
+source /u/msalunkhe/reprocli/.venv/bin/activate
